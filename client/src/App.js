d8c5fa8ea9bc6a5a3d350d2540b657ab29dd2b43
marian.eq <mariano.eq@gmail.com>
2022-06-24 12:47:37 +0200

    Deploy app to Heroku

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("/api");
+      const data = await res.json();
+      setData(data.message);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{data}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
