2e6c6ead3645fbdf634e440272b8c178cce8ab18
marian.eq <mariano.eq@gmail.com>
2022-07-22 21:47:25 +0100

    form able to post request

--- client/src/components/FormRegisterExpense/FormRegisterExpense.js
+++ client/src/components/FormRegisterExpense/FormRegisterExpense.js
@@ -1,37 +1,14 @@
 import React, { useState } from "react";
-
-
-
-
+import styles from "./FormRegisterExpense.module.css";
+import axios from "axios";
 export default function FormRegisterExpense() {
-  const [expenses, setExpenses] = useState([]);
   const [data, setData] = useState({
     concept: "",
     amount: "",
-    selection: "",
     date: "",
+    type: "",
   });
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    addExpense(data);
-  }
-  function addExpense(data) {
-    let newExpense = [...expenses];
-
-    newExpense = [
-      ...newExpense,
-
-      {
-        concept: data.concept,
-        amount: data.amount,
-        selection: data.selection,
-        date: data.date,
-      },
-    ];
-    setExpenses(newExpense);
-  }
-
+  console.log(data);
   function handleChange(event) {
     event.preventDefault();
     let name = event.target.name;
@@ -41,34 +18,61 @@ export default function FormRegisterExpense() {
     });
   }
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const res = await axios.post(`http://localhost:3000/expense`, data);
+    alert("Transaction submitted");
+    console.log(res);
+    return res;
+  }
+
   return (
-    <div>
-      <form>
-        <input
-          type="text"
-          value={data.concept}
-          name="concept"
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          value={data.amount}
-          name="amount"
-          onChange={handleChange}
-        />
-        <select value={data.selection} name="selection" onChange={handleChange}>
-          <option value="income">Income</option>
-          <option value="outcome">Outcome</option>
-        </select>
-        <input
-          type="date"
-          value={data.date}
-          name="date"
-          onChange={handleChange}
-        ></input>
-        <button onClick={handleSubmit}>Submit</button>
+    <div className={styles["form-container"]}>
+      <form onSubmit={handleSubmit}>
+        <div className={styles["form-inputs-container"]}>
+          <label>Concept</label>
+          <input
+            type="text"
+            value={data.concept}
+            name="concept"
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles["form-inputs-container"]}>
+          <label>Amount</label>
+          <input
+            type="number"
+            value={data.amount}
+            name="amount"
+            onChange={handleChange}
+          />
+        </div>
+        <div className={styles["form-inputs-container"]}>
+          <label>Date</label>
+          <input
+            type="date"
+            value={data.date}
+            name="date"
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div className={styles["form-inputs-container"]}>
+          <label>Type</label>
+          <select
+            id="type"
+            value={data.type}
+            name="type"
+            onChange={handleChange}
+          >
+            <option value="">-- Choose --</option>
+            <option value="income">Income</option>
+            <option value="outcome">Outcome</option>
+          </select>
+        </div>
+        <button className={styles["btn-submit-form"]} type="submit">
+          Submit
+        </button>
       </form>
- 
     </div>
   );
 }
