2ead0551c698d5c07dc28b1c3097283ada7b9763
marian.eq <mariano.eq@gmail.com>
2022-07-22 21:46:01 +0100

    Fetc and display data from db

--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+import axios from "axios";
 
-export default function MainPage({ expenses }) {
+export default function MainPage() {
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    const { data } = await axios.get(`http://localhost:3000/expense`);
+    setData(data.payload);
+    console.log(data.payload);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div className="list-container">
-      {expenses.map((e, i) => (
-        <div key={i}>
-          <h3>{e.concept}</h3>
-          <h3>{e.amount}</h3>
-          <h3>{e.date}</h3>
-          <h3>{e.selection}</h3>
-          <button>Delete</button>
-          <button>Edit</button>
-        </div>
-      ))}
+    <div className="[style.table-container]">
+      <table>
+        <tbody>
+          <tr>
+            <th>Concept</th>
+            <th>Amount</th>
+            <th>Type</th>
+            <th>Date</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </tbody>
+        {data.map((element, i) => (
+          <tfoot key={i}>
+          <tr>
+            <td>{element.concept}</td>
+            <td>{element.amount}</td>
+            <td>{element.date}</td>
+            <td>{element.type}</td>
+            <td>
+              <button>Edit</button>
+            </td>
+            <td>
+              <button>Delete</button>
+            </td>
+          </tr>
+          </tfoot>
+        ))}
+        </table>
     </div>
   );
 }
-
