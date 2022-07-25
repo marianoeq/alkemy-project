c90b0888761b9dfecb92fb11787c2d4874c84110
marian.eq <mariano.eq@gmail.com>
2022-07-25 20:47:30 +0100

    Edit functionality added

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import NavBar from "./components/NavBar/NavBar";
 import FormRegisterExpense from "./components/FormRegisterExpense/FormRegisterExpense";
 import MainPage from "./components/MainPage/MainPage";
-/* import NotFound from "./components/NotFound/NotFound"; */
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
-
   const [data, setData] = useState({
     concept: "",
     amount: 0,
     date: "",
     type: "",
   });
+  const sessionID = sessionStorage.getItem("id");
+  const navigate = useNavigate();
 
   function handleChange(event) {
     event.preventDefault();
@@ -25,29 +27,60 @@ function App() {
     });
   }
 
-  function handleEdit(e){
-    e.preventDefault();
-    console.log(data)
-  }
+  //POST REQUEST
+  const handlePost = async (event) => {
+    event.preventDefault();
+    const res = await axios.post(`http://localhost:3000/expense`, data);
+    alert("Transaction submitted");
+    setData("");
+    navigate("/");
+    return res;
+  };
 
-    async function handlePost(event) {
-      event.preventDefault();
-      const res = await axios.post(`http://localhost:3000/expense`, data);
-      alert("Transaction submitted");
-      return res;
-    } 
-  
-  
+  //PUT REQUEST
+  const handleEdit = async (event, id) => {
+    event.preventDefault();
+    const res = await axios.put(`http://localhost:3000/expense/${id}`, data);
+    navigate("/");
+    setData("");
+    return res;
+  };
+
   return (
     <div className="App">
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route exact path="/" element={<MainPage />} />
-          <Route exact path="/formregisterexpense" element={<FormRegisterExpense nameButton={"Submit"} handleSubmit={handlePost} handleChange={handleChange} data={data}/>} />
-          <Route exact path="/formeditexpense" element={<FormRegisterExpense nameButton={"Edit"} handleSubmit={()=>handleEdit()} data={data}/>} />
-        </Routes>
-      </Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
+
+        <Route
+          exact
+          path="/formregisterexpense"
+          element={
+            <FormRegisterExpense
+              nameButton={"Submit"}
+              handleSubmit={handlePost}
+              handleChange={handleChange}
+              data={data}
+              isEditable={true}
+            />
+          }
+        />
+        <Route
+          exact
+          path="/formeditexpense"
+          element={
+            <FormRegisterExpense
+              nameButton={"Edit"}
+              handleSubmit={(e) => handleEdit(e, sessionID)}
+              handleChange={handleChange}
+              data={data}
+              isEditable={false}
+            />
+          }
+        />
+
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
