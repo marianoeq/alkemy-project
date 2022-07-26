b34f41621da938fac2626c0c46706038f00d9183
marian.eq <mariano.eq@gmail.com>
2022-07-26 18:54:22 +0100

    Auth0 Compnent added

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,19 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Auth0Provider
+  domain="dev-mlebn30d.us.auth0.com"
+  clientId="ll1b76E1D7MtUmM2Stnm4CXYczuyk1yN"
+  redirectUri={window.location.origin}
+>
   <BrowserRouter>
+  
     <App />
   </BrowserRouter>
+  </Auth0Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
