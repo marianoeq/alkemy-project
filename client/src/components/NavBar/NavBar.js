f4850ea9778a3dc7b1ba57a4a903aa7208831620
marian.eq <mariano.eq@gmail.com>
2022-07-26 12:44:59 +0100

    NavBar styled

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,20 +1,30 @@
-
-import styles from "./NavBar.module.css"
-
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   return (
-   <header>
-    <div className={styles.siteTitle}>
-        <h1>Track expense</h1>
-    </div>
-    <nav>
+    <header className={styles["navbar-container"]}>
+      <div className={styles["title-container"]}>
+        <h1 className={styles["site-title"]}>Track Your Expenses</h1>
+      </div>
+      <nav>
         <ul>
-            <li><a href="/">Main Page</a></li>
-            <li><a href="/formregisterexpense">Register expense</a></li>
-            <li>Log in/sign up</li>
+          <li>
+            <button>
+              <a href="/">Main Page</a>
+            </button>
+          </li>
+          <li>
+            <button>
+              <a href="/formregisterexpense">Register expense</a>
+            </button>
+          </li>
+          <li>
+            <button>
+              <a href="*">Log in/sign up</a>
+            </button>
+          </li>
         </ul>
-    </nav>
-   </header>
+      </nav>
+    </header>
   );
 }
