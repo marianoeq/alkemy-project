04f9847f95e7e65d0dc6f032fd3e497f1e1e910a
marian.eq <mariano.eq@gmail.com>
2022-07-27 13:07:25 +0100

    Implementing auth0

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { Auth0Provider } from "@auth0/auth0-react";
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Auth0Provider
-  domain="dev-mlebn30d.us.auth0.com"
-  clientId="ll1b76E1D7MtUmM2Stnm4CXYczuyk1yN"
-  redirectUri={window.location.origin}
->
   <BrowserRouter>
-  
-    <App />
+    <Auth0Provider
+      domain="dev-mlebn30d.us.auth0.com"
+      clientId="ll1b76E1D7MtUmM2Stnm4CXYczuyk1yN"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
+    ,
   </BrowserRouter>
-  </Auth0Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
